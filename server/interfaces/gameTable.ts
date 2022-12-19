0176508a064d3e6778aaa2b08691fddd9beba3d8
Maxim Peturkin <maxpayne7@yandex.ru>
2022-12-20 01:38:33 +0300

    gameTable impl

--- server/interfaces/gameTable.ts
+++ server/interfaces/gameTable.ts
@@ -1,3 +1,13 @@
-export interface IGameTableShape {
-  
-}
+import { DevDeck } from '../modules/DevDeck';
+import { EDevDeckLevel, IDevDeckShape } from './devDeck';
+
+export type TGameTableShape<C> = {
+  [key in EDevDeckLevel]: {
+    deck: IDevDeckShape<C>;
+    cards: Array<C>;
+  };
+};
+
+export type TGameTableConfig<C> = {
+  [key in EDevDeckLevel]: Array<C>;
+};
